6da4f298219d761a9e866a54a01373f080c8c4f5
Anton Blokhin <blokhin.aa17@physics.msu.ru>
2026-03-09 18:19:24 +0300

    iter26: handle syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -32,7 +32,7 @@ func main() {
 	}
 
 	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 
 	errCh := make(chan error, 1)
 
